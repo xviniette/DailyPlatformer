f97f7b5324ccc097cbe855b3018e827f815be1e7
xviniette <xviniette@gmail.com>
2016-05-09 17:22:29 +0200

    fs + correction mysql
    :facepunch:

--- server/mysql.js
+++ server/mysql.js
@@ -18,22 +18,6 @@ module.exports = function(app){
         }
     });
 
-    var getUserById = function(id, callback){
-        db.query("SELECT * FROM users WHERE id = ?;", [id], callback);
-    }
-
-    var getUserByLogin = function(login, callback){
-        db.query("SELECT * FROM users WHERE login = ?;", [login], callback);
-    }
-
-    var getUserAuthentification = function(login, password, callback){
-        db.query("SELECT * FROM users WHERE login = ? AND password = ?;", [login, password], callback);
-    }
-
-    var getUserByToken = function(token, callback){
-        db.query("SELECT * FROM users WHERE token = ?;", [token], callback);
-    }
-
     return {
         user:{
             getUserById:function(id, callback){
