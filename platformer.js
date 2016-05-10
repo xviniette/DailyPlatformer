2fac1d9807222201ef5861eccf3d340c0f4d3d5f
xviniette <xviniette@gmail.com>
2016-05-10 18:05:16 +0200

    Runs

--- platformer.js
+++ platformer.js
@@ -17,8 +17,6 @@ app.use(express.static("public"));
 
 var routes = require('./server/routes.js')(app);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3000);
 
 
--- server/routes/run.js
+++ server/routes/run.js
@@ -1,5 +1,8 @@
 module.exports = function(app, baseURL){
     var mysql = app.get("MysqlManager");
 
-    
+    app.get(baseURL+"/bests/:map/:ranked?/:limit?", function(req, res){
+        console.log(req.params.map, req.params.ranked, req.params.limit);
+        res.json(req.params);
+    });
 }
